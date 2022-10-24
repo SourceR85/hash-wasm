de7d7d174371dff685fafb404070c594af1bcb1d
Steven-John Lange <s.lange@hz-deutschland.de>
2022-10-24 19:53:46 +0200

    chore: bring buildscripts to 2022

--- scripts/optimize.js
+++ scripts/optimize.js
@@ -1,14 +1,14 @@
 /* eslint-disable import/no-extraneous-dependencies */
 /* eslint-disable no-console */
-import fs from 'node:fs';
+import { readFileSync, writeFileSync } from 'node:fs';
 import binaryen from 'binaryen';
 
 console.log('binaryen optimize start');
 
-const mod = binaryen.readBinary(fs.readFileSync('./wasm/bcrypt.wasm'));
+const mod = binaryen.readBinary(readFileSync('./wasm/bcrypt.wasm'));
 mod.optimize();
 
 const wasmData = mod.emitBinary();
-fs.writeFileSync('./wasm/bcrypt.wasm', wasmData);
+writeFileSync('./wasm/bcrypt.wasm', wasmData);
 
 console.log('binaryen optimize done');
